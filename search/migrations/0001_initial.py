46053b2f9e004af3a5cbbd5e484809388cd610fb
Shivam Kohli <shivamkohli@Shivams-MacBook-Pro.local>
2016-09-30 03:24:45 +0530

    added a game

--- search/migrations/0001_initial.py
+++ search/migrations/0001_initial.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('pokemon_name', models.CharField(unique=True, max_length=128)),
                 ('pokemon_type', models.CharField(max_length=128)),
-                ('pokemon_image', models.CharField(max_length=128)),
             ],
             options={
             },
--- search/models.py
+++ search/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Pokedex(models.Model):
-	pokemon_name = models.CharField(max_length=128, unique=True)
-	pokemon_type = models.CharField(max_length=128)
-	#now we will will also show the image of the pokemon
-	pokemon_image = models.CharField(max_length=128)
+    pokemon_name = models.CharField(max_length=128, unique=True)
+    pokemon_type = models.CharField(max_length=128)
+    pokemon_image = models.CharField(max_length=128)
 
-	def __unicode__(self):  
-		return self.pokemon_name
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.pokemon_name
